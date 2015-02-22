217e39030163f2e8a49328485fb3941c47c32b67
Mikhail Panshenskov <mpansh@gmail.com>
2015-02-23 00:55:11 +0300

    Change default port to 80

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.get('/history/latest/:id', history.getLatestStamp);
 app.get('/history/all/property/:property', history.getAllProperties);
 
 	
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(80);
+console.log('Listening on port 80...');
